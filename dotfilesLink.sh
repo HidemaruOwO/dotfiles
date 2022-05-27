428acdb66a1b603cfd850d79d2a87abd632fce98
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-05-28 02:13:23 +0900

    シンボリックリンク循環の対策

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,3 +5,7 @@ ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 ln -sf ~/dotfiles/.config/coc ~/.config/coc
 ln -sf ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+
+# UNIXではlnコマンドに-Tオプションが存在しない
+rm -rf ~/dotfiles/.config/nvim/nvim
+rm -rf ~/dotfiles/.config/coc/coc
