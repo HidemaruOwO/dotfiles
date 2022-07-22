5e2755344abb8131b20f1e7965d4ec0d9a9d1cc4
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-07-22 20:10:15 +0900

    renamed directory

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,17 +6,17 @@ fi
 ln -sf ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.config/nvim ~/.config
-ln -sf ~/dotfiles/.config/coc ~/.config
-ln -sf ~/dotfiles/.config/fish ~/.config
-ln -sf ~/dotfiles/.config/wezterm ~/.config
-ln -sf ~/dotfiles/.config/fcitx ~/.config
-ln -sf ~/dotfiles/.oh-my-zsh ~
+ln -sf ~/dotfiles/config/nvim ~/.config
+ln -sf ~/dotfiles/config/coc ~/.config
+ln -sf ~/dotfiles/config/fish ~/.config
+ln -sf ~/dotfiles/config/wezterm ~/.config
+ln -sf ~/dotfiles/config/fcitx ~/.config
+ln -sf ~/dotfiles/oh-my-zsh/* ~/.oh-my-zsh
 
 # UNIXではlnコマンドに-Tオプションが存在しない
-rm -rf ~/dotfiles/.config/nvim/nvim
-rm -rf ~/dotfiles/.config/coc/coc
-rm -rf ~/dotfiles/.oh-my-zsh/.oh-my-zsh
-rm -rf ~/dotfiles/.config/fish/fish
-rm -rf ~/dotfiles/.config/wezterm/wezterm
-rm -rf ~/dotfiles/.config/fcitx/fcitx
+rm -rf ~/dotfiles/config/nvim/nvim
+rm -rf ~/dotfiles/config/coc/coc
+rm -rf ~/dotfiles/oh-my-zsh/.oh-my-zsh
+rm -rf ~/dotfiles/config/fish/fish
+rm -rf ~/dotfiles/config/wezterm/wezterm
+rm -rf ~/dotfiles/config/fcitx/fcitx
