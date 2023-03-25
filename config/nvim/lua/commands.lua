5dcebc363064b613ab9f5d6a9dd15c391da5d701
HidemaruOwO <hidemal@outlook.jp>
2023-03-25 14:57:00 +0900

    :sparkles: LazyGitコマンドの追加 (config/nvim/lua/commands.lua)

--- config/nvim/lua/commands.lua
+++ config/nvim/lua/commands.lua
@@ -1,1 +1,5 @@
-vim.api.nvim_create_user_command('FernToggle', 'Fern . -reveal=% -drawer -toggle -width=33', {})
+local api = vim.api
+
+api.nvim_create_user_command('LazyGit',
+  ':Deol -split=floating -winheight=35 -winwidth=120 -winrow=11.5 -wincol=45 -command=lazygit', {})
+api.nvim_create_user_command('FernToggle', 'Fern . -reveal=% -drawer -toggle -width=33', {})
