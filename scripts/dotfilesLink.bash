2454554fbd57b6f078d1df5ca376faf56b8a35a2
HidemaruOwO <hidemal@outlook.jp>
2023-08-27 16:57:52 +0900

    ✨ add dotfiles link v1 (scripts/dotfilesLink.bash)

--- scripts/dotfilesLink.bash
+++ scripts/dotfilesLink.bash
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo -e "\e[1m┌──────────────────────────────────────┐\e[0m"
+echo -e "\e[1m│   💫 Dotfiles Link V1                │\e[0m"
+echo -e "\e[1m└──────────────────────────────────────┘\e[0m"
+
+
+ASAHI=0
+
 directories=("$HOME/.oh-my-zsh" "$HOME/.config" "$HOME/.local" "$HOME/.local/share")
 
 for dir in "${directories[@]}"; do
@@ -8,10 +15,13 @@ for dir in "${directories[@]}"; do
   fi
 done
 
-configs=(
+dots=(
   ".p10k.zsh"
   ".vimrc"
   ".zshrc"
+  )
+
+configs=(
   "nvim"
   "coc"
   "fish"
@@ -28,13 +38,50 @@ configs=(
   "input-remapper-2"
 )
 
+asahiconfigs=(
+  "waybar"
+  "hypr"
+)
+
 dotfiles_dir=$HOME/dotfiles
 
-for config in "${configs[@]}"; do
-  echo "${dotfiles_dir}/config/${config}"
-  if [ -e "${dotfiles_dir}/config/${config}" ]; then
-    echo "exist"
-    ln -snfv "${dotfiles_dir}/config/${config}" "$HOME/.config"
+if [[ "$(uname -r)" == *"asahi"* ]]; then
+      echo -e "\e[1;33m🌞 This is Asahi Linux !!\e[0m"
+      ASAHI=1
+fi
+
+echo "┌──────────────────────────────────────┐"
+echo "│   📌 Sync dots                       │"
+echo "└──────────────────────────────────────┘"
+
+for config in "${dots[@]}"; do
+
+  echo -e "🌟 \e[1mSynchronized..\e[0m \e[1;35m${dotfiles_dir}/${config}\e[0m"
+  if [ -e "${dotfiles_dir}/${config}" ]; then
+    echo -e "'$HOME/.config/$config' -> '${dotfiles_dir}/config/${config}"
+  else
+    ln -snfv "${dotfiles_dir}/${config}" "$HOME/.config"
   fi
 done
+
+echo "┌──────────────────────────────────────┐"
+echo "│   💎 Sync configs                    │"
+echo "└──────────────────────────────────────┘"
+
+for config in "${configs[@]}"; do
+  echo -e "🌟 \e[1mSynchronized..\e[0m \e[1;35m${dotfiles_dir}/config/${config}\e[0m"
+  if [ -e "${dotfiles_dir}/config/${config}" ]; then
+    echo "'$HOME/.config/$config' -> '${dotfiles_dir}/config/${config}"
+  else
+      if [[ "${asahiconfigs[*]}" == *"${config}"* ]] && [ "$ASAHI" == 1 ] ; then
+        echo -e "\e[1;33m🌞 This is Asahi Linux !!\e[0m"
+        echo -e "🌟 \e[1mSynchronized..\e[0m \e[1;35m${dotfiles_dir}/config/${config}-asahi\e[0m"
+        ln -snfv "${dotfiles_dir}/config/${config}-asahi" "$HOME/.config/${config}"
+        rm "$HOME/.config/$config/${config}-asahi"
+      else
+        ln -snfv "${dotfiles_dir}/config/${config}" "$HOME/.config"
+      fi
+  fi
+done
+
 
