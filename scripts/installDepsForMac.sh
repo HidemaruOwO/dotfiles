cc72ad7f78f8de9cf3087e74bf6fe83a77b3e086
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2023-10-29 17:42:07 +0900

    Update installDepsForMac.sh

--- scripts/installDepsForMac.sh
+++ scripts/installDepsForMac.sh
@@ -2,3 +2,4 @@
 brew tap homebrew/cask-versions
 brew install fish bash zsh vivaldi neovim wezterm visual-studio visual-studio-code github gh discord kitty zoom slack google-chrome obs obs-virtualcam thefuck karabiner-elements neovide wget lazygit ripgrep the_silver_searcher got p7zip deluge btop bottom sqlite firefox floorp bat exa lsd figlet toilet deno node go openjdk@17 openjdk@8 rust zig lua php composer ruby julia zls gopls rust-analyzer pipx npm yarn pnpm eslint prettier prettierd isort black stylua shfmt codespell
 brew install starship neofetch hyfetch
+brew install bitwarden
