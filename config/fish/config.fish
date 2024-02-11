3ba3a51b2e6e80e9b89377f09395ace1849998ef
HidemaruOwO <hidemal@outlook.jp>
2024-02-11 20:50:42 +0900

    ✨ I dont need neofetch (config/fish/config.fish)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     set -x PATH /opt/homebrew/bin $PATH
     set -x PATH /opt/homebrew/sbin $PATH
-    hyfetch --ascii-file ~/dotfiles/templates/neofetch/ascii-slime-1.txt
+    # hyfetch --ascii-file ~/dotfiles/templates/neofetch/ascii-slime-1.txt
     echo "__        __   _                            _           __ _     _     _ _
 \ \      / /__| | ___ ___  _ __ ___   ___  | |_ ___    / _(_)___| |__ | | |
  \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \  | |_| / __| '_ \| | |
