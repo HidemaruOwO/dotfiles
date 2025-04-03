67106ef02c8de7bef57ee5b3a6f30d8bcfe7a1f5
HidemaruOwO <owner@v-sli.me>
2025-04-03 13:31:59 +0900

    🗑️ disable TAB completion (config/nvim/lua/plugin/settings/cmp.lua)

--- config/nvim/lua/plugin/settings/cmp.lua
+++ config/nvim/lua/plugin/settings/cmp.lua
@@ -19,10 +19,10 @@ cmp.setup({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
-		["<TAB>"] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}),
+		-- ["<TAB>"] = cmp.mapping.confirm({
+		-- 	behavior = cmp.ConfirmBehavior.Replace,
+		-- 	select = true,
+		-- }),
 	}),
 	sources = cmp.config.sources({
 		{ name = "path" },
