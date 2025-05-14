bdaf550655c31bfb0a6efcc0f4fdef36e2aadcf9
HidemaruOwO <owner@v-sli.me>
2025-05-15 08:24:47 +0900

    ✨ enable core.filetypes (config/nvim/init.lua)

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.cmd([[runtime ./autoload/jetpack.vim]])
 require("core.options")
 require("core.keymaps")
 require("core.commands")
+require("core.filetypes")
 
 -- load plugins
 require("plugins.jetpack")
