310e2458ea18129cd72b265821d054b3f475e96e
HidemaruOwO <owner@v-sli.me>
2025-05-16 14:04:09 +0900

    🔧 disable autotag with tree sitter (config/nvim/lua/plugins/treesitter.lua)

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -23,7 +23,7 @@ require("nvim-treesitter.configs").setup({
 		max_file_lines = nil, -- Do not enable for files with more than n lines, int
 	},
 	autotag = {
-		enable = true,
+		enable = false,
 	},
 })
 
