632255d89b33512f07df540ce582b6cfc662cc96
HidemaruOwO <owner@v-sli.me>
2025-07-31 20:12:41 +0900

    🔧 update (config/fish/config.fish, config/fish/fish_variable...)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,6 +19,7 @@ if status is-interactive
     fish_add_path $HOME/go/bin
     fish_add_path $HOME/.bun/bin
     fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/bin
     fish_add_path $HOME/.bin
     fish_add_path $HOME/.cargo/bin
     # set variable
