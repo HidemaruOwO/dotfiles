457783fa9f43115c858c2e2de37a7cbb58ec7283
HidemaruOwO <owner@v-sli.me>
2025-11-29 12:20:38 +0900

    🔧 books update (config/nvim/lua/core/keymaps.lua)

--- config/nvim/lua/core/keymaps.lua
+++ config/nvim/lua/core/keymaps.lua
@@ -5,6 +5,7 @@ vim.api.nvim_set_keymap("n", "<C-c>", "<Cmd>CommentToggle<CR>", opt)
 vim.api.nvim_set_keymap("v", "<C-c>", ":'<,'>CommentToggle<CR>", opt)
 -- telescope
 vim.api.nvim_set_keymap("n", "<C-s>", "<Cmd>Telescope current_buffer_fuzzy_find<CR>", opt)
+vim.api.nvim_set_keymap("n", "<C-S-s>", "<Cmd>Telescope grep_string<CR>", opt)
 vim.api.nvim_set_keymap("n", "<C-m>", "<Cmd>Telescope file_browser path=%:p:h select_buffer=true<CR><ESC>", opt)
 vim.api.nvim_set_keymap("n", "<C-S-m>", "<Cmd>Telescope frecency<CR><ESC>", opt)
 vim.api.nvim_set_keymap("n", "<C-.>", "<Cmd>Telescope symbols<CR>", opt)
